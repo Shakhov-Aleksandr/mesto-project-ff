18213e4c307101063c05397ecf2a0c9bd1b934c7
Shakhov Aleksandr <None-Value@yandex.ru>
2025-05-13 19:50:08 +0300

    fix: createCard function is fixed

--- scripts/index.js
+++ scripts/index.js
@@ -6,12 +6,13 @@ const content = document.querySelector('.content');
 const placeList = content.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function addCard(name, link) {
+function createCard(name, link, handler) {
     const cardElement = card.querySelector('.card').cloneNode(true);
     cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__image').alt = name;
     cardElement.querySelector('.card__title').textContent = name;
-    cardElement.querySelector('.card__delete-button').addEventListener('click',  evt => deleteCard(evt));
-    placeList.append(cardElement);
+    cardElement.querySelector('.card__delete-button').addEventListener('click', handler );
+    return cardElement;
 }
 
 // @todo: Функция удаления карточки
@@ -20,4 +21,6 @@ function deleteCard(evt) {
 };
 
 // @todo: Вывести карточки на страницу
-initialCards.forEach(card => addCard(card.name, card.link));
+initialCards.forEach(card =>  placeList.append(createCard(card.name, card.link, evt => deleteCard(evt))));
+   
+
