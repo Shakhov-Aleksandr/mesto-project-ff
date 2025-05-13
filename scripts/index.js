381555ccc6f9802c472f3a94909c2a65618c3424
Shakhov Aleksandr <None-Value@yandex.ru>
2025-05-13 16:46:37 +0300

    feat: the function for deleting a card has been added

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,22 @@
 // @todo: Темплейт карточки
-const content = document.querySelector('.content');
-const placeList = content.querySelector('.places__list');
+const card = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
-const card = document.querySelector('#card-template').content;
-const cardDeleteButton = card.querySelector('.card__delete-button');
-const cardLikeButton = card.querySelector('.card__like-button')
+const content = document.querySelector('.content');
+const placeList = content.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 function addCard(name, link) {
     const cardElement = card.querySelector('.card').cloneNode(true);
-
     cardElement.querySelector('.card__image').src = link;
     cardElement.querySelector('.card__title').textContent = name;
-
-    cardDeleteButton.addEventListener('click', deleteCard);
-
+    cardElement.querySelector('.card__delete-button').addEventListener('click',  evt => deleteCard(evt));
     placeList.append(cardElement);
 }
 
 // @todo: Функция удаления карточки
-const deleteCard = function() {
-    
-    cardElement.remove();
+function deleteCard(evt) {
+    evt.target.closest('.places__item').remove();
 };
 
 // @todo: Вывести карточки на страницу
